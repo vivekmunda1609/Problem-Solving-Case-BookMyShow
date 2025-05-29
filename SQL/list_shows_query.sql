c7262feff89e857997bc15bc7f26777b1b2783e9
vivekmunda1609 <96060260+vivekmunda1609@users.noreply.github.com>
2025-05-29 16:17:24 +0530

    Update list_shows_query.sql

--- SQL/list_shows_query.sql
+++ SQL/list_shows_query.sql
@@ -1,1 +1,18 @@
+SELECT 
+    M.title AS movie_title,
+    M.language,
+    M.format,
+    S.show_time,
+    S.screen_type
+FROM 
+    Showtime S
+JOIN 
+    Movie M ON S.movie_id = M.movie_id
+JOIN 
+    Theatre T ON S.theatre_id = T.theatre_id
+WHERE 
+    T.name = 'PVR: Nexus (Forum)'
+    AND S.show_date = '2023-04-25'
+ORDER BY 
+    S.show_time;
 
